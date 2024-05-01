b68d02c784a4d32fdadb5f76c626f65b4bb62a26
xALEXANDREYx <127264834+xALEXANDREYx@users.noreply.github.com>
2024-05-01 17:12:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
+let novaSenha = "";
+sizePassword.textContent = sliderElement.value;
+sliderElement.oninput = function() {
+  sizePassword.textContent = this.value;
+}
+
+function generatePassword() {
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  console.log(pass);
+  containerPassword.classList.remove("hide");
+  password.textContent = pass;
+  novaSenha = pass;
+}
+
+function copyPassword() {
+  alert("Senha copiada com sucesso!");
+  navigator.clipboard.writeText(novaSenha);
+}
